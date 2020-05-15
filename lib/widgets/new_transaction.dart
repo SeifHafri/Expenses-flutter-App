1683385e18325a1a3480200d2cb74db63997e59c
Seif <is_hafri@esi.dz>
2020-05-15 13:43:17 +0100

    modal bottom view enhanced

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -44,44 +44,47 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            TextField(
-              onSubmitted: (_) => _submitData(),
-              controller: _itemController,
-              decoration: InputDecoration(labelText: "Item"),
-            ),
-            TextField(
-              onSubmitted: (_) => _submitData(),
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: "Amount"),
-              controller: _amountController,
-            ),
-            Container(
-              height: 70,
-              
-              child: Row(
-                children: <Widget>[
-                  Expanded(child: Text(_selectedDate==null ? 'No date chosen' : "Date choisie: ${DateFormat.yMd().format(_selectedDate)}")),
-                  FlatButton(child: Text("Choose Date",style:  TextStyle(fontWeight:  FontWeight.bold),),textColor: Theme.of(context).primaryColor,onPressed: _presentDatePicker,)
-                ],
+    return SingleChildScrollView(
+      
+          child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(top:10,left: 10,right: 10,bottom: MediaQuery.of(context).viewInsets.bottom + 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                onSubmitted: (_) => _submitData(),
+                controller: _itemController,
+                decoration: InputDecoration(labelText: "Item"),
               ),
-            ),
-            RaisedButton(
-              shape: new RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(16.0)),
-              onPressed: _submitData,
-              child: Text("Add transaction"),
-              // textColor: Theme.of(context).textTheme.button.color,
-              textColor: Colors.white,
-              color: Colors.purple,
-            )
-          ],
+              TextField(
+                onSubmitted: (_) => _submitData(),
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: "Amount"),
+                controller: _amountController,
+              ),
+              Container(
+                height: 70,
+                
+                child: Row(
+                  children: <Widget>[
+                    Expanded(child: Text(_selectedDate==null ? 'No date chosen' : "Date choisie: ${DateFormat.yMd().format(_selectedDate)}")),
+                    FlatButton(child: Text("Choose Date",style:  TextStyle(fontWeight:  FontWeight.bold),),textColor: Theme.of(context).primaryColor,onPressed: _presentDatePicker,)
+                  ],
+                ),
+              ),
+              RaisedButton(
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(16.0)),
+                onPressed: _submitData,
+                child: Text("Add transaction"),
+                // textColor: Theme.of(context).textTheme.button.color,
+                textColor: Colors.white,
+                color: Colors.purple,
+              )
+            ],
+          ),
         ),
       ),
     );
